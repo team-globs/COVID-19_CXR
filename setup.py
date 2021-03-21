28cdea94dde3f972fe22699de2922293f2b4b32f
Michail Mamalakis <mixalis.mamalakis@gmail.com>
2021-03-21 11:14:10 +0000

    Update setup.py
    v0.2

--- setup.py
+++ setup.py
@@ -14,39 +14,35 @@ setup(name='covid_pipeline',
       license='GPL-3.0+',
       packages=['covid_pipeline'],
       install_requires=[
-	  'niftynet',
-#	  'tf-nightly-gpu',
-#	  'tf-nightly',
-	  'h5py',
+          'niftynet',
+          'seaborn',
+          'h5py',
           'numpy>=1.15.4',
           'scipy>=1.1.0',
-          'matplotlib==3.0.2',
-	  'dicom',
-	  'pydicom==1.2.1',	
-	  'opencv-python==4.0.0.21',
-	  'Pillow==5.3.0',
-	  'vtk>=8.1.1',	
+          'matplotlib>=3.1.0',
+          'dicom',
+          'pydicom>=2.1.1',
+          'opencv-python>=4.0.0.21',
+          'Pillow==8.1.2',
+          'vtk>=7.2.0',
           'future',
-          'keras==2.3.0',
-          'tensorflow>=2.0.0b1',
-          'tensorboard>=2.0.0',
-	  'tensorflow-gpu>=2.0.0b1',
-	  'rq-scheduler==0.7.0',
-	  'med2image',
-	  'imageio',
+          'keras==2.4.1',
+          'tensorflow>=2.4.1',
+          'tensorboard>=2.4.1',
+          'tensorflow-gpu>=2.4.1',
+          'rq-scheduler>=0.7.0',
+          'med2image',
+          'imageio',
           'gensim',
           'SimpleItk',
           'networkx',
           'sklearn',
+
       ],
 	zip_safe=False
 
 )
 
 
-#nano ../.conda-sharc/envs/covid/lib/python3.6/site-packages/keras/backend/tensorflow_backend.py
-#add from tensorflow.python.types import core as core_tf_types
-#modify def is_tensor(x):
-#modify    return isinstance(x, core_tf_types.Tensor) or tf_ops.is_dense_tensor_like(x)
 
 
